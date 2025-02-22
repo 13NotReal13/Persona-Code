8cdd52854498913efe86b4a826b56e13f6993eac
Ivan Semikin <i.semikin.94@icloud.com>
2025-02-22 18:31:38 +0100

    Initial Commit

---
+++ Persona Code/Persona_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Persona_CodeApp.swift
+//  Persona Code
+//
+//  Created by Иван Семикин on 22/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Persona_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
