dd465348c8970157c809760544ea94d2dc49edf4
Ivan Semikin <i.semikin.94@icloud.com>
2025-03-01 16:08:38 +0100

    Fix font in HelpInfoView

--- Persona Code/Screens/HelpInfo/Views/HelpInfoCategoriesView.swift
+++ Persona Code/Screens/HelpInfo/Views/HelpInfoCategoriesView.swift
@@ -12,35 +12,25 @@ struct HelpInfoCategoriesView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 8) {
-                ForEach(categories, id: \.title) { category in
-                    Text(category.title)
-                        .foregroundStyle(.white)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-            }
-            
             VStack(spacing: 16) {
                 ForEach(categories, id: \.title) { category in
                     // Заголовок категории
                     Text(category.title)
-                        .font(.system(size: 21))
+                        .font(.title2)
                         .bold()
-                        .foregroundStyle(.white)
-                        .padding(.top)
                     
-                    VStack(spacing: 16) {
+                    VStack(alignment: .leading, spacing: 16) {
                         ForEach(category.subcategories, id: \.title) { subcategory in
                             VStack(alignment: .leading, spacing: 4) {
                                 // Заголовок подкатегории
                                 Text(subcategory.title)
-                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                                    .bold()
                                 // Описание подкатегории
                                 Text(subcategory.shortDescription)
                                     .foregroundStyle(.white.opacity(0.8))
                             }
                         }
-                        .frame(maxWidth: .infinity, alignment: .leading)
                     }
                 }
             }
